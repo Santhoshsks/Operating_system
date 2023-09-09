3db73920ca12594e1e7602b0c17e0b62bce99c33
santhosh <93921543+Santhoshsks@users.noreply.github.com>
2023-09-09 11:30:20 +0530

    Update exec_using_strings.py

--- System_calls/exec_using_strings.py
+++ System_calls/exec_using_strings.py
@@ -15,3 +15,10 @@ if __name__ == "__main__":
 	exec(b.s1)
 
 #2nd file named as b.py
+def fac(num):
+	ans = 1
+	for i in range(1,num+1):
+		ans *= i
+	return ans
+
+s1 = '''import a\nn = int(input("Enter n for a:"))\nprint(fac(n))\nexec(a.s)'''
